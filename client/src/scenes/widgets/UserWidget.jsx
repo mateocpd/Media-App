52c5432499c1c054b06256af577c267694305f7a
Mateo <mateo.capdevila2801@gmail.com>
2023-06-02 20:05:14 -0300

    profile widget

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -12,7 +12,7 @@ import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const UserWidget = ({ userId, picturePath }) => {
   const [user, setUser] = useState(null);
@@ -56,7 +56,7 @@ const UserWidget = ({ userId, picturePath }) => {
       <FlexBetween
         gap="0.5rem"
         pb="1.1rem"
-        onClick={() => Navigate(`/profile/${userId}`)}
+        onClick={() => navigate(`/profile/${userId}`)}
       >
         <FlexBetween gap="1rem">
           <UserImage image={picturePath} />
@@ -76,8 +76,10 @@ const UserWidget = ({ userId, picturePath }) => {
             </Typography>
             <Typography color={medium}>{friends.length} friends</Typography>
           </Box>
-          <ManageAccountsOutlined />
         </FlexBetween>
+          <ManageAccountsOutlined />
+      </FlexBetween>
+
 
         <Divider />
         {/*Second Row */}
@@ -91,6 +93,7 @@ const UserWidget = ({ userId, picturePath }) => {
             <Typography color={medium}>{occupation}</Typography>
           </Box>
         </Box>
+        <Divider />
 
         {/*Third Row */}
         <Box p="1rem 0">
@@ -101,13 +104,49 @@ const UserWidget = ({ userId, picturePath }) => {
             </Typography>
           </FlexBetween>
           <FlexBetween>
-          <Typography color={medium}> Impressions of your post</Typography>
+            <Typography color={medium}> Impressions of your post</Typography>
             <Typography color={main} fontWeight="500">
-              {viewedProfile}
+              {impressions}
             </Typography>
           </FlexBetween>
         </Box>
-      </FlexBetween>
+
+        <Divider />
+
+        {/*Fourth Row */}
+        <Box p="1rem 0">
+          <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+            Social Profiles
+          </Typography>
+
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Social Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOulined sx={{ color: main }} />
+          </FlexBetween>
+
+          <FlexBetween gap="1rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/linkedin.png" alt="linkedin" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  LinkedIn
+                </Typography>
+                <Typography color={medium}>Network Platform</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOulined sx={{ color: main }} />
+          </FlexBetween>
+        </Box>
     </WidgetWrapper>
   );
 };
+
+export default UserWidget;
